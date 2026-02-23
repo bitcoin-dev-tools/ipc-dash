28c743827bb27b529dab405ba5be7276ec8e6d6b
will <will@256k1.dev>
2026-02-23 14:51:02 +0000

    Add client-only IPC archive to packaged SDK

--- ipc_exporter/bitcoin-ipc-sdk.nix
+++ ipc_exporter/bitcoin-ipc-sdk.nix
@@ -43,6 +43,43 @@ stdenv.mkDerivation rec {
       fi
     done
 
+    # Build a client-only IPC archive to avoid pulling wallet IPC server code.
+    ipc_objdir="${bitcoinBuildDir}/src/ipc/CMakeFiles/bitcoin_ipc.dir"
+    if [ -d "$ipc_objdir" ]; then
+      cp \
+        "$ipc_objdir/interfaces.cpp.o" \
+        "$ipc_objdir/process.cpp.o" \
+        "$ipc_objdir/capnp/common.cpp.o" \
+        "$ipc_objdir/capnp/protocol.cpp.o" \
+        "$ipc_objdir/capnp/node.cpp.o" \
+        "$ipc_objdir/capnp/chain.cpp.o" \
+        "$ipc_objdir/capnp/common.capnp.c++.o" \
+        "$ipc_objdir/capnp/common.capnp.proxy-client.c++.o" \
+        "$ipc_objdir/capnp/common.capnp.proxy-server.c++.o" \
+        "$ipc_objdir/capnp/common.capnp.proxy-types.c++.o" \
+        "$ipc_objdir/capnp/handler.capnp.c++.o" \
+        "$ipc_objdir/capnp/handler.capnp.proxy-client.c++.o" \
+        "$ipc_objdir/capnp/handler.capnp.proxy-server.c++.o" \
+        "$ipc_objdir/capnp/handler.capnp.proxy-types.c++.o" \
+        "$ipc_objdir/capnp/init.capnp.c++.o" \
+        "$ipc_objdir/capnp/init.capnp.proxy-client.c++.o" \
+        "$ipc_objdir/capnp/init.capnp.proxy-server.c++.o" \
+        "$ipc_objdir/capnp/init.capnp.proxy-types.c++.o" \
+        "$ipc_objdir/capnp/node.capnp.c++.o" \
+        "$ipc_objdir/capnp/node.capnp.proxy-client.c++.o" \
+        "$ipc_objdir/capnp/node.capnp.proxy-server.c++.o" \
+        "$ipc_objdir/capnp/node.capnp.proxy-types.c++.o" \
+        "$ipc_objdir/capnp/chain.capnp.c++.o" \
+        "$ipc_objdir/capnp/chain.capnp.proxy-client.c++.o" \
+        "$ipc_objdir/capnp/chain.capnp.proxy-server.c++.o" \
+        "$ipc_objdir/capnp/chain.capnp.proxy-types.c++.o" \
+        "$ipc_objdir/capnp/echo.capnp.c++.o" \
+        "$ipc_objdir/capnp/mining.capnp.c++.o" \
+        "$ipc_objdir/capnp/wallet.capnp.c++.o" \
+        "$TMPDIR/"
+      ar rcs "$out/lib/libbitcoin_ipc_client.a" "$TMPDIR/"*.o
+    fi
+
     cat > "$out/nix-support/cmake-flags" <<EOF_INNER
 -DBITCOIN_SDK_DIR=$out
 EOF_INNER
