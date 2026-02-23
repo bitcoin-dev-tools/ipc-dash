c4f2a1c5b662f739f5bd06247ead2f7b6b399e1e
will <will@256k1.dev>
2026-02-23 15:40:54 +0000

    Remove Rust build artifacts and ignore local build outputs

--- ipc_exporter_rust/target/debug/build/ipc-exporter-rust-a1695182c0cea9f8/build_script_build-a1695182c0cea9f8.d
+++
@@ -1,5 +0,0 @@
-/home/will/src/bitcoin-dev-tools/usdt-dash/ipc_exporter_rust/target/debug/build/ipc-exporter-rust-a1695182c0cea9f8/build_script_build-a1695182c0cea9f8.d: build.rs
-
-/home/will/src/bitcoin-dev-tools/usdt-dash/ipc_exporter_rust/target/debug/build/ipc-exporter-rust-a1695182c0cea9f8/build_script_build-a1695182c0cea9f8: build.rs
-
-build.rs:
